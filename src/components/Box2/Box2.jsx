53eb399a0182e3248163aa9b6561a7694e952f2a
zinminhtun <zinminhtunok591997@gmail.com>
2022-04-15 12:53:51 +0630

    practise framer-motion

---
+++ src/components/Box2/Box2.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { motion } from "framer-motion";
+
+const Box2 = () => {
+    const [isAnimating,setIsAnimating]=useState(false)
+  return (
+      <div className="box-container">
+          <motion.div
+              className='box'
+              drag
+              dragConstraints={{
+                  right: 20,
+                  left: -20,
+                  top: 5,
+                  bottom:5
+              }}
+              whileHover={{
+                  scale:1.1
+              }}
+              whileTap={{
+                  scale:0.9
+              }}
+            
+          >
+
+          </motion.div>
+    </div>
+  )
+}
+
+export default Box2
